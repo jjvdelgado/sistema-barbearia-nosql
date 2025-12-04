5f02cfa216983388712cad5f7e4fc6dd84245ede
João Delgado <joao.victor@loopert.com>
2025-12-04 15:32:43 -0300

    main

--- sql/create_mongodb_structure.py
+++ sql/create_mongodb_structure.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 from pymongo import MongoClient, ASCENDING, DESCENDING
 
 def criar_estrutura_mongodb():
